0551e336b02c4a73370c8c6d498c286e5ba70546
James Johnson <jtaylorjohnson@gmail.com>
2020-04-06 02:48:42 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,16 +10,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    
-    if quantity > 1
-      counter = 0
-      while counter < quantity
-        @items << item
-        counter += 1
-      end
-    else
-      @items << item
-    end
+    quantity.times 
   end
 
   def apply_discount
